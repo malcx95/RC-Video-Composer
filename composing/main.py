8c3b7252fb7fce8ee80c7b3b9e8243c964039dd6
Malcolm Vigren <malvi108@student.liu.se>
2017-05-28 22:21:26 +0200

    Got gizeh working

--- composing/main.py
+++ composing/main.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 
+import gizeh
 import moviepy.editor as edit
+
+W, H = 128, 128
+
+def make_frame(t):
+   surface = gizeh.Surface(W,H, bg_color=None)
+   radius = W * (1 + (t * (2 - t)) ** 2 ) / 6
+   circle = gizeh.circle(radius, xy = (W / 2,H / 2), fill=(1, 0, 0))
+   circle.draw(surface)
+   return surface.get_npimage()
+
+
+def write_file(clip, output_file):
+    clip.write_videofile(output_file)
+    
 
 def main():
     clip1 = edit.VideoFileClip("./example-video/EXAMPLE1.MOV")
-    clip2 = edit.VideoFileClip("./example-video/EXAMPLE2.MOV")
-    final = edit.concatenate_videoclips([clip1, clip2])
-    final.write_videofile("out/out.mp4")
+    graphics = edit.VideoClip(make_frame, duration=clip1.duration)
+    # clip2 = edit.VideoFileClip("./example-video/EXAMPLE2.MOV")
+    # final = edit.concatenate_videoclips([clip1, clip2])
+
+    write_file(edit.CompositeVideoClip([clip1, graphics.set_position((100, 100))]), "out/out.mp4")
 
 if __name__ == "__main__":
     main()
